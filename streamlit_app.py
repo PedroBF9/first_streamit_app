83d9cfa48f21fe0ce787abe3289f20310a50f683
PedroBF9 <137057143+PedroBF9@users.noreply.github.com>
2023-06-19 12:24:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 
 streamlit.title("My Parents New Healthy Diner")
-streamlit.header("Omega 3 & Blueberry Smoothie")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
+streamlit.header(🥣 "Omega 3 & Blueberry Smoothie")
+streamlit.text(🥗 "Kale, Spinach & Rocket Smoothie")
+streamlit.text(🐔 'Hard-Boiled Free-Range Egg')
+streamlit.text(🥑🍞 'Avocado Toast')
 
